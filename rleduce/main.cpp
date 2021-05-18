3d80ff41cb5af0d686c44bb0dcefdf25b513aac5
Andrew <andrew@digerati.co.nz>
2021-05-19 10:29:57 +1200

    Initial Commit

---
+++ rleduce/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rleduce
+//
+//  Created by Andrew Simmonds on 19/05/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
