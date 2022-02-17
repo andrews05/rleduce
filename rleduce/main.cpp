fe12ee274e680d245c678114c09b147163c151e6
Andrew <andrew@digerati.co.nz>
2022-02-17 17:09:15 +1300

    Add options

--- rleduce/main.cpp
+++ rleduce/main.cpp
@@ -14,6 +14,14 @@
 #include "libGraphite/rsrc/file.hpp"
 using namespace graphite;
 
+static struct options {
+    bool trim = false;
+    bool reduce = false;
+    bool create = false;
+    bool dither = true;
+    bool verbose = false;
+} options;
+
 enum rleop: uint8_t {
     eof = 0x00,
     line_start = 0x01,
@@ -22,7 +30,7 @@ enum rleop: uint8_t {
     pixel_run = 0x04,
 };
 
-std::size_t processRle(std::shared_ptr<rsrc::resource> resource, bool resize) {
+int64_t processRle(std::shared_ptr<rsrc::resource> resource) {
     auto reader = data::reader(resource->data());
     auto width = reader.read_short();
     auto height = reader.read_short();
@@ -35,7 +43,7 @@ std::size_t processRle(std::shared_ptr<rsrc::resource> resource, bool resize) {
     int32_t count;
     int trim = 0;
     // Figure out how many lines can be trimmed from top/bottom of the whole sprite
-    if (resize) {
+    if (options.trim) {
         trim = height/2;
         for (int i=0; i<frames; i++) {
             int line = 0;
@@ -119,7 +127,7 @@ void applyError(std::shared_ptr<qd::surface> surface, int x, int y, int errors[3
     surface->set(x, y, qd::color(comps[0], comps[1], comps[2], color.alpha_component()));
 }
 
-void ditherSurface(std::shared_ptr<qd::surface> surface) {
+void rgb555dither(std::shared_ptr<qd::surface> surface) {
     // QuickDraw dithering algorithm.
     // Half the error is diffused right on even rows, left on odd rows. The remainder is diffused down.
     auto frameWidth = surface->size().width();
@@ -129,12 +137,12 @@ void ditherSurface(std::shared_ptr<qd::surface> surface) {
         for (int w=0; w<frameWidth; w++) {
             int x = even ? w : frameWidth - w - 1;
             auto color = surface->at(x, y);
-            int comps[3] = { color.red_component(), color.green_component(), color.blue_component() };
-            int errors[3];
-            for (int i=0; i<3; i++) {
-                int newComp = (comps[i] & 0xF8) * 0xFF / 0xF8;
-                errors[i] = comps[i] - newComp;
-            }
+            auto newColor = qd::color(color.rgb555());
+            int errors[3] = {
+                color.red_component() - newColor.red_component(),
+                color.green_component() - newColor.green_component(),
+                color.blue_component() - newColor.blue_component()
+            };
             if (errors[0] || errors[1] || errors[2]) {
                 if (even && x+1 < frameWidth) {
                     applyError(surface, x+1, y, errors, false);
@@ -149,12 +157,15 @@ void ditherSurface(std::shared_ptr<qd::surface> surface) {
     }
 }
 
-std::size_t processPict(std::shared_ptr<rsrc::resource> resource, bool rgb555) {
+int64_t processPict(std::shared_ptr<rsrc::resource> resource) {
     qd::pict pict(resource->data());
-    ditherSurface(pict.image_surface().lock());
-    auto data = pict.data(rgb555);
+    if (options.reduce && options.dither) {
+        rgb555dither(pict.image_surface().lock());
+    }
+    auto data = pict.data(options.reduce);
     auto diff = resource->data()->size() - data->size();
-    if (diff <= 0) {
+    // Force write if format is non-standard (QuickTime)
+    if (diff <= 0 && pict.format() <= 32) {
         return 0;
     }
     resource->set_data(data);
@@ -169,23 +180,34 @@ bool processSpin(std::shared_ptr<rsrc::resource> resource, rsrc::file file) {
     auto frameY = reader.read_short();
     auto gridX = reader.read_short();
     auto gridY = reader.read_short();
+    if (frameX <= 0 || frameY <= 0 || gridX <= 0 || gridY <= 0) {
+        std::cerr << "Invalid spïn resource: " << resource->id() << std::endl;
+        return false;
+    }
     
     auto spriteRes = file.find("PICT", spriteID, {}).lock();
     auto maskRes = file.find("PICT", maskID, {}).lock();
-    if (spriteRes == nullptr || maskRes == nullptr || frameX <= 0 || frameY <= 0 || gridX <= 0 || gridY <= 0) {
+    if (spriteRes == nullptr || maskRes == nullptr) {
         return false;
     }
+    
     auto spritePict = qd::pict(spriteRes->data());
-    auto maskPict = qd::pict(maskRes->data());
     auto sprite = spritePict.image_surface().lock();
+    if (sprite->size().width() != frameX * gridX || sprite->size().height() != frameY * gridY) {
+        std::cerr << "Sprite PICT " << spriteID << " for spïn " << resource->id() << " does not match grid size." << std::endl;
+        return false;
+    }
+    auto maskPict = qd::pict(maskRes->data());
     auto mask = maskPict.image_surface().lock();
-    if (sprite->size().width() < frameX * gridX || sprite->size().height() < frameY * gridY
-        || mask->size().width() < frameX * gridX || mask->size().height() < frameY * gridY) {
+    if (mask->size().width() != frameX * gridX || mask->size().height() != frameY * gridY) {
+        std::cerr << "Mask PICT " << maskID << " for spïn " << resource->id() << " does not match grid size." << std::endl;
         return false;
     }
     
     auto rle = qd::rle(qd::size(frameX, frameY), gridX * gridY);
-    ditherSurface(sprite);
+    if (options.dither) {
+        rgb555dither(sprite);
+    }
     for (int gy=0; gy<gridY; gy++) {
         for (int gx=0; gx<gridX; gx++) {
             auto frame = std::make_shared<qd::surface>(qd::surface(frameX, frameY));
@@ -205,8 +227,8 @@ bool processSpin(std::shared_ptr<rsrc::resource> resource, rsrc::file file) {
     return true;
 }
 
-bool processFile(std::string path, bool resize, bool rgb555) {
-    auto filename = std::filesystem::path(path).filename();
+bool processFile(std::filesystem::path path, std::filesystem::path outpath) {
+    auto filename = path.filename();
     rsrc::file file;
     try {
         file = rsrc::file(path);
@@ -217,21 +239,30 @@ bool processFile(std::string path, bool resize, bool rgb555) {
     std::cout << "Processing " << filename << "..." << std::endl;
     bool writeFile = false;
     for (auto typeList : file.types()) {
-        std::size_t saved = 0;
+        int64_t saved = 0;
         if (typeList->code() == "rlëD") {
             for (auto resource : typeList->resources()) {
-                saved += processRle(resource, resize);
+                saved += processRle(resource);
             }
             std::cout << "Saved " << saved << " bytes from " << typeList->count() << " rlëDs." << std::endl;
         } else if (typeList->code() == "PICT") {
             for (auto resource : typeList->resources()) {
-                saved += processPict(resource, rgb555);
+                try {
+                    saved += processPict(resource);
+                } catch (const std::exception& e) {
+                    std::cerr << e.what() << std::endl;
+                }
             }
             std::cout << "Saved " << saved << " bytes from " << typeList->count() << " PICTs." << std::endl;
-        } else if (typeList->code() == "spïn") {
+        } else if (typeList->code() == "spïn" && options.create) {
             for (auto resource : typeList->resources()) {
-                processSpin(resource, file);
+                try {
+                    saved += processSpin(resource, file);
+                } catch (const std::exception& e) {
+                    std::cerr << e.what() << std::endl;
+                }
             }
+            std::cout << "Created " << saved << " rlëDs from " << typeList->count() << " spïns." << std::endl;
         }
         if (saved) {
             writeFile = true;
@@ -241,28 +272,94 @@ bool processFile(std::string path, bool resize, bool rgb555) {
         std::cout << "No changes written." << std::endl;
         return false;
     }
-    file.write(path+".rez", file.current_format());
+    auto format = file.current_format();
+    if (outpath.empty()) {
+        outpath = path;
+    } else {
+        auto ext = outpath.extension();
+        if (ext == ".rez") {
+            format = rsrc::file::format::rez;
+        } else if (ext == ".ndat" || ext == ".npif" || ext == ".rsrc") {
+            format = rsrc::file::format::classic;
+        }
+    }
+    file.write(outpath, format);
     return true;
+}
+
+void printUsage() {
+    std::cerr << "Usage: rleduce [options] file ..." << std::endl;
+    std::cerr << "  -t --trim           allow rlëD frame trimming (smaller output)" << std::endl;
+    std::cerr << "  -r --reduce         reduce PICT depth to 16-bit (smaller output)" << std::endl;
+    std::cerr << "  -c --create         create rlëDs from spïns with PICTs" << std::endl;
+    std::cerr << "  -n --no-dither      don't dither when reducing to 16-bit" << std::endl;
+    std::cerr << "  -o --output <path>  set output file/directory" << std::endl;
+    std::cerr << "  -v --verbose        enable verbose output" << std::endl;
+}
+
+void processOption(std::string arg) {
+    if (arg == "t" || arg == "--trim") {
+        options.trim = true;
+    } else if (arg == "r" || arg == "--reduce") {
+        options.reduce = true;
+    } else if (arg == "c" || arg == "--create") {
+        options.create = true;
+    } else if (arg == "n" || arg == "--no-dither") {
+        options.dither = false;
+    } else if (arg == "v" || arg == "--verbose") {
+        options.verbose = true;
+    } else {
+        std::cerr << "Unknown option: " << arg << std::endl;
+        printUsage();
+        exit(1);
+    }
 }
 
 int main(int argc, const char * argv[]) {
     if (argc < 2) {
         std::cerr << "Optimize the size of rlëD and PICT resources in resource files." << std::endl;
-        std::cerr << "Usage: rleduce [-f] file ..." << std::endl;
-        std::cerr << "  -f  allow rlëD frame resizing (further optimization)" << std::endl;
-        std::cerr << "  -d  reduce PICT depth to 16-bit (further optimization)" << std::endl;
+        printUsage();
         return 1;
     }
-    bool resize = false;
-    bool rgb555 = false;
+    std::vector<std::filesystem::path> files;
+    std::filesystem::path outpath;
+    bool outdir = false;
     for (int i=1; i<argc; i++) {
-        if (strcmp(argv[i], "-f") == 0) {
-            resize = true;
-        } else if (strcmp(argv[i], "-d") == 0) {
-            rgb555 = true;
+        std::string arg(argv[i]);
+        if (arg[0] == '-' && arg.size() > 1) {
+            if (arg == "-o" || arg == "--output") {
+                if (++i == argc) {
+                    std::cerr << arg << " option requires a value." << std::endl;
+                    return 1;
+                }
+                outpath = std::filesystem::path(argv[i]);
+                if (std::filesystem::is_directory(outpath)) {
+                    outdir = true;
+                } else {
+                    auto parent = outpath.parent_path();
+                    if (!std::filesystem::is_directory(parent)) {
+                        std::cerr << "Output directory " << parent << " does not exist." << std::endl;
+                        return 1;
+                    }
+                }
+            } else if (arg[1] == '-') {
+                processOption(arg);
+            } else {
+                for (int j=1; j<arg.size(); j++) {
+                    processOption(arg.substr(j, 1));
+                }
+            }
         } else {
-            processFile(std::string(argv[i]), resize, rgb555);
+            files.emplace_back(std::filesystem::path(arg));
         }
+    }
+    if (!files.size()) {
+        std::cerr << "No files provided." << std::endl;
+        printUsage();
+        return 1;
+    }
+    for (auto file : files) {
+        processFile(file, outdir ? outpath / file.filename() : outpath);
     }
     return 0;
 }
